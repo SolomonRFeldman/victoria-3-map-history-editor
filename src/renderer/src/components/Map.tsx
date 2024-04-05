a539eb2ddc6869affde5da64727f3dc4f226e47c
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2024-04-05 16:39:23 -0400

    cleanup

--- src/renderer/src/components/Map.tsx
+++ src/renderer/src/components/Map.tsx
@@ -1,5 +1,5 @@
-import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
-import { CRS, LatLngBoundsExpression } from 'leaflet';
+import { MapContainer, ImageOverlay } from 'react-leaflet'
+import { CRS, LatLngBoundsExpression } from 'leaflet'
 import flatmap from '../assets/flatmap_votp.png'
 import land_mask from '../assets/land_mask.png'
 import flatmap_overlay from '../assets/flatmap_overlay_votp.png'
@@ -9,7 +9,7 @@ const bounds: LatLngBoundsExpression = [[0, 0], [3616, 8192]]
 
 export default function Map() {
   return (
-    <MapContainer center={[0, 0]} minZoom={-3} maxZoom={2} doubleClickZoom={false} crs={CRS.Simple} bounds={bounds}>
+    <MapContainer center={[0, 0]} minZoom={-2} maxZoom={2} doubleClickZoom={false} crs={CRS.Simple} bounds={bounds}>
       <ImageOverlay url={flatmap} bounds={bounds} />
       <ImageOverlay url={land_mask} bounds={bounds} />
       <ImageOverlay url={flatmap_overlay} bounds={bounds} />
