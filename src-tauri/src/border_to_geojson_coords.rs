869e779c4b11a6d5837435bf81d71bf2740204f9
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2024-04-09 12:01:04 -0400

    fixes spacing

--- src-tauri/src/border_to_geojson_coords.rs
+++ src-tauri/src/border_to_geojson_coords.rs
@@ -1,9 +1,9 @@
 #[derive(Clone)]
 enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
+  Up,
+  Right,
+  Down,
+  Left,
 }
 
 struct Rotation {
@@ -12,7 +12,7 @@ struct Rotation {
 
 impl Rotation {
   fn new() -> Self {
-      Self { position: 2 }
+    Self { position: 2 }
   }
 
   fn cycle(&self) -> [Direction; 4] {
@@ -25,19 +25,19 @@ impl Rotation {
   }
 
   fn cycle_forward(&mut self) {
-      if self.position + 1 < self.cycle().len() {
-          self.position += 1;
-      } else {
-          self.position = 0;
-      }
+    if self.position + 1 < self.cycle().len() {
+      self.position += 1;
+    } else {
+      self.position = 0;
+    }
   }
 
   fn cycle_backward(&mut self) {
-      if self.position == 0 {
-          self.position = self.cycle().len() - 1;
-      } else {
-          self.position -= 1;
-      }
+    if self.position == 0 {
+      self.position = self.cycle().len() - 1;
+    } else {
+      self.position -= 1;
+    }
   }
 
   fn position_name(&self) -> Direction {
@@ -46,18 +46,18 @@ impl Rotation {
 
   fn x_modifier(&self) -> i32 {
     match self.position_name() {
-        Direction::Right => 1,
-        Direction::Left => -1,
-        _ => 0,
+      Direction::Right => 1,
+      Direction::Left => -1,
+      _ => 0,
     }
   }
 
   fn y_modifier(&self) -> i32 {
-      match self.position_name() {
-          Direction::Up => 1,
-          Direction::Down => -1,
-          _ => 0,
-      }
+    match self.position_name() {
+      Direction::Up => 1,
+      Direction::Down => -1,
+      _ => 0,
+    }
   }
 
   fn next_coord(&self, coord: (i32, i32)) -> (i32, i32) {
@@ -91,64 +91,63 @@ pub fn border_to_geojson_coords(border_coords: Vec<(i32, i32)>) -> Vec<(i32, i32
 
     let next_coord = rotation.next_coord(current_coord);
     if hash_coords.contains(&next_coord) {
-        geo_trace.push(next_coord);
-        if next_coord == origin_coord {
-            break;
-        }
-        current_coord = next_coord;
-        continue;
+      geo_trace.push(next_coord);
+      if next_coord == origin_coord {
+        break;
+      }
+      current_coord = next_coord;
+      continue;
     } else {
-        rotation.cycle_forward();
+      rotation.cycle_forward();
     }
 
     let next_coord = rotation.next_coord(current_coord);
     if hash_coords.contains(&next_coord) {
-        geo_trace.push(next_coord);
-        if next_coord == origin_coord {
-            break;
-        }
-        current_coord = next_coord;
-        continue;
+      geo_trace.push(next_coord);
+      if next_coord == origin_coord {
+        break;
+      }
+      current_coord = next_coord;
+      continue;
     } else {
-        rotation.cycle_backward();
-        rotation.cycle_backward();
+      rotation.cycle_backward();
+      rotation.cycle_backward();
     }
     
     let next_coord = rotation.next_coord(current_coord);
     if hash_coords.contains(&next_coord) {
-        geo_trace.push(next_coord);
-        if next_coord == origin_coord {
-            break;
-        }
-        current_coord = next_coord;
+      geo_trace.push(next_coord);
+      if next_coord == origin_coord {
+        break;
+      }
+      current_coord = next_coord;
     }
   }
 
   remove_unnecessary_coords(geo_trace)
 }
 
-
 fn remove_unnecessary_coords(geo_trace: Vec<(i32, i32)>) -> Vec<(i32, i32)> {
   let mut new_geo_trace: Vec<Option<(i32, i32)>> = vec![Some(geo_trace[0])];
 
   for i in 1..geo_trace.len() - 1 {
-      let coord = geo_trace[i];
-      let backward_coord = geo_trace[i - 1];
-      let forward_coord = geo_trace[i + 1];
-  
-      let backward_y_diff = (coord.0 - backward_coord.0).signum();
-      let backward_x_diff = (coord.1 - backward_coord.1).signum();
-      let forward_y_diff = (forward_coord.0 - coord.0).signum();
-      let forward_x_diff = (forward_coord.1 - coord.1).signum();
-  
-      if (backward_y_diff, backward_x_diff) == (forward_y_diff, forward_x_diff) {
-          new_geo_trace.push(None);
-      } else {
-          new_geo_trace.push(Some(coord));
-      }
+    let coord = geo_trace[i];
+    let backward_coord = geo_trace[i - 1];
+    let forward_coord = geo_trace[i + 1];
+
+    let backward_y_diff = (coord.0 - backward_coord.0).signum();
+    let backward_x_diff = (coord.1 - backward_coord.1).signum();
+    let forward_y_diff = (forward_coord.0 - coord.0).signum();
+    let forward_x_diff = (forward_coord.1 - coord.1).signum();
+
+    if (backward_y_diff, backward_x_diff) == (forward_y_diff, forward_x_diff) {
+      new_geo_trace.push(None);
+    } else {
+      new_geo_trace.push(Some(coord));
+    }
   }
-  
+
   new_geo_trace.push(Some(*geo_trace.last().unwrap()));
-  
+
   new_geo_trace.into_iter().filter_map(|x| x).collect()
 }
