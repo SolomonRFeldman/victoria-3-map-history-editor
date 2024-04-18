1bd51ca52d49f8b7db226e0d44babcc8b1aa9313
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2024-04-17 23:08:35 -0400

    extracts map background

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,13 @@
-import { MapContainer, ImageOverlay } from 'react-leaflet'
+import { MapContainer } from 'react-leaflet'
 import { CRS, LatLngBoundsExpression, LeafletMouseEvent } from 'leaflet'
 import './Map.css'
 import { useEffect, useState } from 'react'
 import { listen } from '@tauri-apps/api/event'
-import { appCacheDir, join } from '@tauri-apps/api/path'
-import { exists } from '@tauri-apps/api/fs'
-import { convertFileSrc, invoke } from '@tauri-apps/api/tauri'
-import Countries from './Countries'
-import States from './States'
+import { invoke } from '@tauri-apps/api/tauri'
+import Countries, { Country } from './Countries'
+import States, { State } from './States'
 import Provinces from './Provinces'
+import Background from './Background'
 
 export type Coords = [number, number][][]
 
@@ -20,44 +19,15 @@ type StateCoords = {
   [key: string]: Coords
 }
 
-type Country = {
-  name: string,
-  color: string,
-  coordinates: Coords,
-  states: State[]
-}
-
-type State = {
-  name: string,
-  color: string
-  provinces: string[]
-}
-
 type TransferStateResponse = {
   to_country: Country,
   from_country: Country,
   state_coords: Coords
 }
 
-const flatmapFileName = 'flatmap_votp.png'
-const landMaskFileName = 'land_mask.png'
-const flatmapOverlayFileName = 'flatmap_overlay_votp.png'
-
 const bounds: LatLngBoundsExpression = [[0, 0], [3616, 8192]]
 
-const getImagePath = async (filename: string, callback: (path: string | null) => void) => {
-  const cacheDir = await appCacheDir()
-  const path = await join(cacheDir, filename)
-  const fileExists = await exists(path)
-  console.log(`File ${filename} at ${path} exists: ${fileExists}`)
-
-  return fileExists ? callback(convertFileSrc(path)) : null
-}
-
 export default function Map() {
-  const [flatmap, setFlatmap] = useState<null | string>(null)
-  const [flatmapOverlay, setFlatmapOverlay] = useState<null | string>('')
-  const [landMask, setLandMask] = useState<null | string>('')
   const [countries, setCountries] = useState<Country[]>([])
   const [stateCoords, setStateCoords] = useState<StateCoords>({})
   const [provinceCoords, setProvinceCoords] = useState<ProvincesCoords>({})
@@ -66,29 +36,6 @@ export default function Map() {
   const [stateToTransfer, setStateToTransfer] = useState<State | null>(null)
   const [renderBreaker, setRenderBreaker] = useState(Date.now())
   const forceRerender = () => setRenderBreaker(Date.now())
-
-  useEffect(() => {
-    const unlistenToFlatmap = listen<String>('load-flatmap', () => {
-      getImagePath(flatmapFileName, setFlatmap)
-    })
-    getImagePath(flatmapFileName, setFlatmap)
-
-    const unlistenToLandMask = listen<String>('load-land-mask', () => {
-      getImagePath(landMaskFileName, setLandMask)
-    })
-    getImagePath(landMaskFileName, setLandMask)
-
-    const unlistenToFlatmapOverlay = listen<String>('load-flatmap-overlay', () => {
-      getImagePath(flatmapOverlayFileName, setFlatmapOverlay)
-    })
-    getImagePath(flatmapOverlayFileName, setFlatmapOverlay)
-
-    return () => {
-      unlistenToFlatmap.then((unlisten) => unlisten())
-      unlistenToFlatmapOverlay.then((unlisten) => unlisten())
-      unlistenToLandMask.then((unlisten) => unlisten())
-    }
-  }, [])
 
   useEffect(() => {
     const unlistenToProvinceCoords = listen<ProvincesCoords>('load-province-coords', (data) => {
@@ -181,9 +128,7 @@ export default function Map() {
 
   return (
     <MapContainer center={[0, 0]} minZoom={-2} maxZoom={2} doubleClickZoom={false} crs={CRS.Simple} bounds={bounds}>
-      { flatmap && <ImageOverlay url={flatmap} bounds={bounds} /> }
-      { landMask && <ImageOverlay url={landMask} bounds={bounds} /> }
-      { flatmapOverlay && <ImageOverlay url={flatmapOverlay} bounds={bounds} /> }
+      <Background bounds={bounds} />
       <Countries countries={countries} renderBreaker={renderBreaker} eventHandlers={{ click: handleClickCountry }} />
       { selectedCountry && <States country={selectedCountry} stateCoords={stateCoords} renderBreaker={renderBreaker} eventHandlers={{ click: handleClickState }} /> }
       { selectedState && <Provinces state={selectedState} provinceCoords={provinceCoords} renderBreaker={renderBreaker} /> }
